04cae2dad4a188523d9cbc21bd77f3f69a7e0d17
Robson <robsonsiebel@yahoo.com.br>
2020-06-19 17:34:14 +0100

    Add PuzzlePiece with rotation
    - Created simple puzzle piece with rotation on click
    - Imported sprites from kenney.nl
    - Imported LeanTween

---
+++ Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    private readonly float ROTATE_SPEED = 0.15f;
+
+    private bool m_Rotating = false;
+
+    public int StartingRotation { get; set; }
+    public int TargetRotation { get; set; }
+
+    // Events
+    public Action OnPieceMoved;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        Rotate();
+    }
+
+    public void Rotate()
+    {
+        if (m_Rotating)
+            return;
+
+        print("rotation: " + transform.localEulerAngles);
+
+        m_Rotating = true;
+
+        LeanTween.rotateZ(gameObject, transform.localEulerAngles.z - 90, ROTATE_SPEED).setOnComplete(() =>
+        {
+            m_Rotating = false;
+            if (OnPieceMoved != null)
+                OnPieceMoved.Invoke();
+        });
+    }
+}
